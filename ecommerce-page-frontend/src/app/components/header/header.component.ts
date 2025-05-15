2fdc114191be17304d9870c325edd96d42ea66d7
ClementSellyPro <clementselly.pro@gmail.com>
2025-05-15 12:47:05 +0400

    Nav links with hover

--- ecommerce-page-frontend/src/app/components/header/header.component.ts
+++ ecommerce-page-frontend/src/app/components/header/header.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import {NgOptimizedImage} from '@angular/common';
+import {NavBarComponent} from '../nav-bar/nav-bar.component';
 
 @Component({
   selector: 'app-header',
   imports: [
-    NgOptimizedImage
+    NgOptimizedImage,
+    NavBarComponent
   ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
