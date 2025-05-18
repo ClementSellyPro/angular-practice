4a1172ad3b7e5b1494685e23d996252f115c1dd5
ClementSellyPro <clementselly.pro@gmail.com>
2025-05-18 14:22:34 +0400

    Add Gallery component, with style, but without logic

--- ecommerce-page-frontend/src/app/app.component.ts
+++ ecommerce-page-frontend/src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './components/header/header.component';
+import {GalleryComponent} from './components/gallery/gallery.component';
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, GalleryComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ ecommerce-page-frontend/src/app/components/gallery/gallery.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import {NgOptimizedImage} from '@angular/common';
+
+@Component({
+  selector: 'app-gallery',
+  imports: [NgOptimizedImage],
+  templateUrl: './gallery.component.html',
+  styleUrl: './gallery.component.css'
+})
+export class GalleryComponent {
+
+}
