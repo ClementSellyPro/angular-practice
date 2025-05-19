c23c84fccb59bf7f86377a7b1107168f3b9be197
ClementSellyPro <clementselly.pro@gmail.com>
2025-05-19 09:23:09 +0400

    Add conditionnal class to thumbnail when the image is selected

--- ecommerce-page-frontend/src/app/components/gallery/gallery.component.ts
+++ ecommerce-page-frontend/src/app/components/gallery/gallery.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
-import {NgFor, NgOptimizedImage} from '@angular/common';
+import {NgClass, NgOptimizedImage} from '@angular/common';
 import {ImagesType} from '../../models/ImagesType.type';
 
 @Component({
   selector: 'app-gallery',
   imports: [
     NgOptimizedImage,
-    NgFor
+    NgClass
   ],
   templateUrl: './gallery.component.html',
   styleUrl: './gallery.component.css'
@@ -15,10 +15,10 @@ export class GalleryComponent {
   selectedImage: number = 0;
 
   images: ImagesType[] = [
-    {id: 0, largeImg: "assets/images/image-product-1.jpg", thumbImg: "assets/images/image-product-1-thumbnail.jpg"},
-    {id: 1, largeImg: "assets/images/image-product-2.jpg", thumbImg: "assets/images/image-product-2-thumbnail.jpg"},
-    {id: 2, largeImg: "assets/images/image-product-3.jpg", thumbImg: "assets/images/image-product-3-thumbnail.jpg"},
-    {id: 3, largeImg: "assets/images/image-product-4.jpg", thumbImg: "assets/images/image-product-4-thumbnail.jpg"},
+    {id: 0, largeImg: "assets/images/image-product-1.jpg", thumbImg: "assets/images/image-product-1-thumbnail.jpg", },
+    {id: 1, largeImg: "assets/images/image-product-2.jpg", thumbImg: "assets/images/image-product-2-thumbnail.jpg", },
+    {id: 2, largeImg: "assets/images/image-product-3.jpg", thumbImg: "assets/images/image-product-3-thumbnail.jpg", },
+    {id: 3, largeImg: "assets/images/image-product-4.jpg", thumbImg: "assets/images/image-product-4-thumbnail.jpg", },
   ];
 
   selectImage(id: number): void {
