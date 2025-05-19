54f5fdfd4f09cc7b6a9945c488677cb6899b4043
ClementSellyPro <clementselly.pro@gmail.com>
2025-05-19 10:27:01 +0400

    Create DescriptionComponent - Add style, without logic

--- ecommerce-page-frontend/src/app/app.component.ts
+++ ecommerce-page-frontend/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './components/header/header.component';
 import {GalleryComponent} from './components/gallery/gallery.component';
+import {DescriptionComponent} from './components/description/description.component';
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent, GalleryComponent],
+  imports: [HeaderComponent, GalleryComponent, DescriptionComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ ecommerce-page-frontend/src/app/components/description/description.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import {NgOptimizedImage} from '@angular/common';
+
+@Component({
+  selector: 'app-description',
+  imports: [
+    NgOptimizedImage
+  ],
+  templateUrl: './description.component.html',
+  styleUrl: './description.component.css'
+})
+export class DescriptionComponent {
+
+}
