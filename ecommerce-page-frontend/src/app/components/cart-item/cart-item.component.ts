933a057a1a91e327eb024307228b147091f0ebf2
ClementSellyPro <clementselly.pro@gmail.com>
2025-05-21 15:08:55 +0400

    Generate CartItemComponent, and style

---
+++ ecommerce-page-frontend/src/app/components/cart-item/cart-item.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import {NgOptimizedImage} from '@angular/common';
+
+@Component({
+  selector: 'app-cart-item',
+  imports: [
+    NgOptimizedImage
+  ],
+  templateUrl: './cart-item.component.html',
+  styleUrl: './cart-item.component.css'
+})
+export class CartItemComponent {
+
+}
--- ecommerce-page-frontend/src/app/components/modal-cart/modal-cart.component.ts
+++ ecommerce-page-frontend/src/app/components/modal-cart/modal-cart.component.ts
@@ -1,8 +1,9 @@
 import {Component} from '@angular/core';
+import {CartItemComponent} from '../cart-item/cart-item.component';
 
 @Component({
   selector: 'app-modal-cart',
-  imports: [],
+  imports: [CartItemComponent],
   templateUrl: './modal-cart.component.html',
   styleUrl: './modal-cart.component.css'
 })
