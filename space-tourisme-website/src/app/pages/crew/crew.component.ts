25a06b4def11fbad32826d17f79bd28c979203b6
ClementSellyPro <clementselly.pro@gmail.com>
2025-05-26 10:46:24 +0400

    Set data dynamically to Crew page, and make possible to select any crew members

--- space-tourisme-website/src/app/pages/crew/crew.component.ts
+++ space-tourisme-website/src/app/pages/crew/crew.component.ts
@@ -1,17 +1,20 @@
 import { Component, OnInit } from '@angular/core';
-import {NgOptimizedImage} from '@angular/common';
+import {NgClass, NgOptimizedImage, UpperCasePipe} from '@angular/common';
 import {DataService} from '../../services/data.service';
 
 @Component({
   selector: 'app-crew',
   imports: [
-    NgOptimizedImage
+    NgOptimizedImage,
+    UpperCasePipe,
+    NgClass
   ],
   templateUrl: './crew.component.html',
   styleUrl: './crew.component.css'
 })
 export class CrewComponent implements OnInit {
   crew: any[] = [];
+  currentNumber: number = 0;
   currentCrew: any = null;
 
   constructor(private dataService: DataService) {}
@@ -21,6 +24,10 @@ export class CrewComponent implements OnInit {
       this.crew = data.crew;
       this.currentCrew = this.crew[0];
     })
+  }
 
+  crewSelection(number: number): void {
+    this.currentNumber = number;
+    this.currentCrew = this.crew[this.currentNumber];
   }
 }
