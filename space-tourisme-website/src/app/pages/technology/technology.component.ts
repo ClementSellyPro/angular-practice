b2756601f060652d626f4a34c31b0ec0765c5cb5
ClementSellyPro <clementselly.pro@gmail.com>
2025-05-26 15:36:12 +0400

    Set dynamically data for Technology page - Desktop version totally done

--- space-tourisme-website/src/app/pages/technology/technology.component.ts
+++ space-tourisme-website/src/app/pages/technology/technology.component.ts
@@ -1,14 +1,37 @@
-import { Component } from '@angular/core';
-import {NgOptimizedImage} from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import {NgClass, NgOptimizedImage, UpperCasePipe} from '@angular/common';
+import {DataService} from '../../services/data.service';
 
 @Component({
   selector: 'app-technology',
   imports: [
-    NgOptimizedImage
+    NgOptimizedImage,
+    NgClass,
+    UpperCasePipe
   ],
   templateUrl: './technology.component.html',
   styleUrl: './technology.component.css'
 })
-export class TechnologyComponent {
+export class TechnologyComponent implements OnInit {
+  technologies: any[] = [];
+  currentTechnology: any = null;
+  currentTechnologyIndex: number = 0;
 
+  constructor(private dataService: DataService) {};
+
+  ngOnInit(): void {
+    this.dataService.getData().subscribe(data => {
+      this.technologies = data.technology;
+      this.currentTechnology = this.technologies[this.currentTechnologyIndex];
+    });
+  }
+
+  technologySelection(number: number): void {
+    this.currentTechnologyIndex = number;
+    this.updateCurrentTechnology();
+  }
+
+  updateCurrentTechnology(){
+    this.currentTechnology = this.technologies[this.currentTechnologyIndex];
+  }
 }
