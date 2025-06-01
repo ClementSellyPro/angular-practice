eefc698ee1ebe9995f06d6eeac286b96cc8f2146
ClementSellyPro <clementselly.pro@gmail.com>
2025-06-01 11:26:20 +0400

    HeaderComponent done + Nav and RouterLink redict to the specific pages

--- my-book-library/src/app/app.routes.ts
+++ my-book-library/src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import {HomeComponent} from './pages/home/home.component';
+import {AddComponent} from './pages/add/add.component';
+import {ListComponent} from './pages/list/list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'add',
+    component: AddComponent
+  },
+  {
+    path: 'list',
+    component: ListComponent
+  }
+];
